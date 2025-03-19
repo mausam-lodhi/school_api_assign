f5cdf89d7a9917d4eaec44d5b5de9649a1f8e1a5
mausam-lodhi <harprasadlodhi1984@gmail.com>
2025-03-19 23:26:49 +0530

    Update schoolValidation.js

--- backend/validations/schoolValidation.js
+++ backend/validations/schoolValidation.js
@@ -3,11 +3,13 @@ const { body, query } = require("express-validator");
 exports.addSchoolValidation = [
 	body("name").notEmpty().withMessage("Name is required"),
 	body("address").notEmpty().withMessage("Address is required"),
-	body("latitude").isFloat({ min: -90, max: 90 }).withMessage("Latitude must be a valid float between -90 and 90"),
-	body("longitude").isFloat({ min: -180, max: 180 }).withMessage("Longitude must be a valid float between -180 and 180"),
+	body("latitude").notEmpty().withMessage("Latitude is required").isFloat({ min: -90, max: 90 }).withMessage("Latitude must be a valid float between -90 and 90"),
+	body("longitude").notEmpty().withMessage("Longitude is required").isFloat({ min: -180, max: 180 }).withMessage("Longitude must be a valid float between -180 and 180 in add school"),
 ];
 
 exports.listSchoolsValidation = [
-	query("latitude").isFloat({ min: -90, max: 90 }).withMessage("Valid latitude is required"),
-	query("longitude").isFloat({ min: -180, max: 180 }).withMessage("Valid longitude is required"),
+	query("name").notEmpty().withMessage("Name is required"),
+	query("address").notEmpty().withMessage("Address is required"),
+	query("latitude").notEmpty().withMessage("Latitude is required").isFloat({ min: -90, max: 90 }).withMessage("Valid latitude is required"),
+	query("longitude").notEmpty().withMessage("Longitude is required").isFloat({ min: -180, max: 180 }).withMessage("Valid longitude is required"),
 ];
